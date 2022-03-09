1870f624860b56cb97c60a738dedd69703779fc8
Sergey Doggunovich <sharcomovie68@gmail.com>
2022-03-09 22:49:21 +0300

    .

--- test_DSU.py
+++ test_DSU.py
@@ -3,9 +3,17 @@ from dsu import DSU
 
 
 class TestDisjointSet(unittest.TestCase):
+    def setUp(self):
+        self.dsu = DSU(10)
+
     def test_find(self):
-        self.assertEqual(union, 10)
+        for i in range(10):
+            self.assertEqual(self.dsu.find(i), i)
+
+    def test_negative_num(self):
+        self.dsu2 = DSU(-10)
+        self.assertEqual(self.dsu2.parent, [])
 
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == 'main':
+    pass
