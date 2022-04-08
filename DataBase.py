ed79ec3f9c0bd60e91d869c88649f39fa904cd59
Sergey Doggunovich <sharcomovie68@gmail.com>
2022-04-08 10:29:39 +0300

    update project

--- DataBase.py
+++ DataBase.py
@@ -1,26 +1,32 @@
-import sqlite3
+from sqlite3 import connect
 
 
 class DataBase:
-    def save_db(self, data, name):
-        conn = sqlite3.connect(name)
-        cur = conn.cursor()
-        cur.execute("""DROP TABLE IF EXISTS array;""")
-        cur.execute("""CREATE TABLE array(
-           id INT PRIMARY KEY,
-           pos REAL,
-           elem TEXT,
-           next REAL)
-        """)
-        conn.commit()
-        for rw in range(data.length()):
-            p = rw*2
-            cur.execute("""INSERT INTO array(id, pos, elem, next) 
-            VALUES('{ids}', '{poss}', '{elems}', '{nexts}');""".format(ids=rw, poss=p, elems=data.arr[p], nexts=data.arr[p+1]))
-            conn.commit()
+    def __init__(self, name):
+        self.conn = connect(f"{name}")
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXIST unions (id INTEGER PRIMARY KEY AUTOINCREMENT, unions TEXT)")
+        self.conn.commit()
+        cursor.close()
 
-    def load_db(self, path):
-        conn = sqlite3.connect(path)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM array;")
-        return cur
+    def insert(self, data):
+        if data == 'None':
+            data = None
+        cursor = self.conn.cursor()
+        cursor.execute(f"""INSERT INTO unions (unions) VALUES ('{data}')""")
+        self.conn.commit()
+        cursor.close()
+
+    def get_id(self):
+        cursor = self.conn.cursor()
+        list_id = [str(i)[1:-2] for i in cursor.execute("SELECT id FROM unions")]
+        cursor.close()
+        return list_id
+
+    def delete_unions(self):
+        cursor = self.conn.cursor()
+        cursor.execute("DELETE FROM unions")
+        cursor.execute("DELETE FROM sqlite_sequence WHERE name = 'unions'")
+        cursor.execute("UPDATE sqlite_sequence SET seq = 0 WHERE name = 'unions'")
+        self.conn.commit()
+        cursor.close()
