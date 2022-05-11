49ea72e51f41541ffb95089ead1382c5cee1dc1b
Sergey Doggunovich <sharcomovie68@gmail.com>
2022-05-11 20:55:39 +0300

    update project

--- test_profile.py
+++ test_profile.py
@@ -3,7 +3,7 @@ from random import randint
 from dsu import DSU
 
 dsu = DSU()
-test_elem = randint(0, 100)
+test_elem = randint(0, 10000)
 
 for i in range(10000):
     dsu.push(test_elem)
@@ -14,5 +14,5 @@ for j in range(10000):
 for k in range(10000):
     dsu.find(test_elem)
 
-for h in range(10000):
+for h in range(1000000):
     dsu.union(test_elem, test_elem)
